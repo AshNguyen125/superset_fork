6bfe593c812c639c5baabfbfa95094c6488eb25f
Jean Massucatto <massucattoj@gmail.com>
2026-05-31 22:23:40 -0300

    chore(frontend): apply prettier formatting to Select.tsx

--- superset-frontend/packages/superset-ui-core/src/components/Select/Select.tsx
+++ superset-frontend/packages/superset-ui-core/src/components/Select/Select.tsx
@@ -584,7 +584,8 @@ const Select = forwardRef(
               handleSelectAll();
             }}
           >
-            {t('Select all')} {`(${formatNumber('SMART_NUMBER', bulkSelectCounts.selectable)})`}
+            {t('Select all')}{' '}
+            {`(${formatNumber('SMART_NUMBER', bulkSelectCounts.selectable)})`}
           </Button>
           <Button
             type="link"
@@ -601,7 +602,8 @@ const Select = forwardRef(
               handleDeselectAll();
             }}
           >
-            {t('Clear')} {`(${formatNumber('SMART_NUMBER', bulkSelectCounts.deselectable)})`}
+            {t('Clear')}{' '}
+            {`(${formatNumber('SMART_NUMBER', bulkSelectCounts.deselectable)})`}
           </Button>
         </StyledBulkActionsContainer>
       ),
